19a57ad40c521d9f1ef9514f5fa01735415b6c57
sarthak <sarthakchaudhary52@gmail.com>
2020-09-23 21:01:23 +0530

    Initial commit without payment

---
+++ config/secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+   database:'mongodb://root:sarthak@cluster0-shard-00-00.yring.mongodb.net:27017,cluster0-shard-00-01.yring.mongodb.net:27017,cluster0-shard-00-02.yring.mongodb.net:27017/db?ssl=true&replicaSet=atlas-vdjhkt-shard-0&authSource=admin&retryWrites=true&w=majority',
+   port: 3001,
+   secretKey:'Sarthak@#'
+
+}
---
+++ server.js
@@ -0,0 +1,75 @@
+var express        = require("express");
+var morgan         = require("morgan");
+var mongoose       = require('mongoose');
+var bodyParser     = require('body-parser');
+var ejs            = require('ejs');
+var engine         = require('ejs-mate');
+var session        = require('express-session');
+var cookieParser   = require('cookie-parser');
+var flash          = require('express-flash');
+var passport       = require('passport');
+var MongoStore     = require('connect-mongo')(session);
+var User           = require('./models/user');
+var Category       = require('./models/category');
+var secret         = require('./config/secret');
+var cartLength     = require('./middlewares/middleware');
+
+
+var app = express();
+
+
+mongoose.connect(secret.database,{ useNewUrlParser: true, useUnifiedTopology: true }, function(err){
+  if(err){
+    console.log("not conencted to the database ");
+    console.log(err);
+  }else{
+      console.log("Connected to the database");
+  }
+});
+
+//middleware
+app.use(express.static(__dirname +'/public'));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized:true,
+  secret:secret.secretKey,
+  store: new MongoStore({url:secret.database, autoReconnect:true})
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next){
+  res.locals.user = req.user;
+  next();
+});
+app.use(function(req, res, next){
+  Category.find({}, function(err, categories){
+    if(err) return next(err);
+    res.locals.categories = categories;
+    next();
+  });
+});
+app.use(cartLength);
+
+app.engine('ejs', engine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+var mainRoutes  = require('./routes/main');
+var userRoutes  = require('./routes/user');
+var adminRoutes = require('./routes/admin');
+var apiRoutes   = require('./api/api');
+app.use(mainRoutes);
+app.use(userRoutes);
+app.use(adminRoutes);
+app.use('/api',apiRoutes);
+
+app.listen(secret.port, function(err){
+  if(err) throw err;
+  console.log("The server is running on port number : " + secret.port);
+});
